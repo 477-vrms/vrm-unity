4aa79735273e8e7002402a0354e83d40753c4839
brianmlatimer <blatimer@purdue.edu>
2022-01-26 19:11:29 -0500

    CLAW

---
+++ 3D VR arms/Assets/Scripts/NEW 3d Scripts/Claw.cs
@@ -0,0 +1,30 @@
+// UnityScript
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Mathf;
+
+
+
+public class Claw : MonoBehaviour
+{
+    public TriggerGrip Gripper; //gets grip % from a custom script (Grip Percent game object)
+    public Transform finger1;
+    public Transform finger2;
+    public float MaxRotation;
+
+
+    void Update()
+    {
+
+        float amountToRotate = Gripper.getGrip() * (MaxRotation / 100);
+        Debug.Log(amountToRotate);
+        Vector3 newRotation = new Vector3(0, amountToRotate, 0);
+        finger1.transform.localEulerAngles = newRotation;
+        finger2.transform.localEulerAngles = newRotation;
+
+    }
+
+}
+
